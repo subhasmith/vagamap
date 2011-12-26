542533670a371e28047f003ef7a2e2a9f5ff4ddd
matt@zoil.org <matt@zoil.org@510033c4-7111-90c5-1fb2-46c4d9902b77>
2011-12-26 22:44:02 +0000

    redid css style sheets and overall layout

--- trunk/src/base.py
+++ trunk/src/base.py
@@ -4,5 +4,8 @@ Created on Dec 24, 2011
 @author: Matt
 '''
 import os 
+import jinja2
 
 folder = os.path.dirname(__file__)
+jinja_environment = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.join(folder, 'templates')))
