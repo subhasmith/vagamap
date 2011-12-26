d09e9b005464213cc0e48902d4cc9e2e8d2c5310
matt <matt@zoil.org>
2011-12-26 23:20:02 +0000

    implemented provider list page

--- src/base.py
+++ src/base.py
@@ -9,3 +9,12 @@ import jinja2
 folder = os.path.dirname(__file__)
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(folder, 'templates')))
+
+def format_datetime(value, fmt=None):
+    if not fmt:
+        fmt = """%m/%d/%y %H:%M:%S"""
+    if value:
+        return value.strftime(fmt)
+    return ''
+
+jinja_environment.filters['datetime'] = format_datetime
