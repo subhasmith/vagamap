06337078572cec9378c4d7cd7be8b2f49fc0514b
matt@zoil.org <matt@zoil.org@510033c4-7111-90c5-1fb2-46c4d9902b77>
2011-12-27 16:15:49 +0000

    commiting before going to work

--- trunk/src/base.py
+++ trunk/src/base.py
@@ -5,10 +5,12 @@ Created on Dec 24, 2011
 '''
 import os 
 import jinja2
+import logging
 
 folder = os.path.dirname(__file__)
 jinja_environment = jinja2.Environment(
     loader=jinja2.FileSystemLoader(os.path.join(folder, 'templates')))
+logging.info("base folder = '{}'".format(folder))
 
 def format_datetime(value, fmt=None):
     if not fmt:
